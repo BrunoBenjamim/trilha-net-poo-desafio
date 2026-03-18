322c28cc2a24e2966dc6a4e22431fe21f9312aef
Bruno Benjamim <brtj123@gmail.com>
2026-03-18 13:04:02 -0300

    Implementação das classes filhas e ajustes

--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("");
+Console.WriteLine("------------------------------");
+Console.WriteLine("SMARTPHONE NOKIA");
+Smartphone nokia = new Nokia("1234", "1111", "2222", 50);
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WHATSAPP");
+
+Console.WriteLine("");
+Console.WriteLine("------------------------------");
+Console.WriteLine("SMARTPHONE IPHONE");
+Smartphone iphone = new Iphone("2345", "3333", "4444", 85);
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("UBER");
+Console.WriteLine("");
+
